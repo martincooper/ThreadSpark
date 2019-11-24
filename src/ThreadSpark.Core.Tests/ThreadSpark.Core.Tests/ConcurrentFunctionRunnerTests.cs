7618df19549d10f6e7a950e58c25d4a974aeff78
Martin Cooper <martin.cooper@me.com>
2019-11-24 12:12:43 +0000

    Updating Unit Tests

--- src/ThreadSpark.Core.Tests/ThreadSpark.Core.Tests/ConcurrentFunctionRunnerTests.cs
+++ src/ThreadSpark.Core.Tests/ThreadSpark.Core.Tests/ConcurrentFunctionRunnerTests.cs
@@ -1,13 +1,34 @@
-﻿using Xunit;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
 
 namespace ThreadSpark.Core.Tests 
 {
+    [TestFixture]
     public class ConcurrentFunctionRunnerTests
     {
-        [Fact]
+        [Test]
         public void SampleTest()
         {
+            var runner = new ConcurrentFunctionRunner(5);
+
+            var funcs = Enumerable.Range(1, 5).Select((rng, idx) => createFunc(idx));
+            runner.Run(funcs);
+            
+            Console.WriteLine("Finished");
+            
             Assert.True(true);
+        }
+
+        Func<int> createFunc(int idx)
+        {
+            return () =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"Running {idx}.");
+                return 1;
+            };
         }
     }
 }
